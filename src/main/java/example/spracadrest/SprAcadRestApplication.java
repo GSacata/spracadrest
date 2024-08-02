b5a0c7270e46f0addf46c95cd6ff0945a2ccc751
GSacata <gsacata@gmail.com>
2024-08-02 20:56:46 -0300

    first commit

---
+++ src/main/java/example/spracadrest/SprAcadRestApplication.java
@@ -0,0 +1,13 @@
+package example.spracadrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprAcadRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprAcadRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/spracadrest/SprAcadRestApplicationTests.java
@@ -0,0 +1,13 @@
+package example.spracadrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprAcadRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
